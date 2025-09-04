9e258f0e4c44049d523364a1be44d432ac134288
Anghelo Ortiz <anghelotiz@gmail.com>
2025-09-04 17:10:37 -0600

    Se realiza el ejercicio 5

--- task5.js
+++ task5.js
@@ -1,7 +1,34 @@
-
 // Type your code below this line!
+const listado =require('readline');
 
+function FriendList(){
+    this.nombres = []
+}
+const rl= listado.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
+const amigos= new FriendList()
 
-// Type your code above this line!
+rl.question("¿Cuántos amigos vas a listar?",(num) => {
+    const count = parseInt(num)
+    let i = 0
+
+function askName(){
+    if (i < count){
+        rl.question(`Ingresa el nombre ${i +1}: `, (name) => {
+            amigos.nombres.push(name);
+            i++;
+            askName()
+        })
+    } else {
+    console.log(amigos.nombres)
+    rl.close()
+}
+}
+askName()
+})    
+    // Type your code above this line!
+
 
